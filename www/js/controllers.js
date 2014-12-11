3e4ec618202227246bbdba64e801f8c9ff2e68f2
Adam Barnhard <admbarn@gmail.com>
2014-12-11 14:43:39 -0600

    added changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,6 +34,36 @@
         },function(){
             console.log('ERROR IN NOTES');
         });
+
+        function success(b64){
+            //console.log(b64);
+            Note.upload(noteId, b64).then(function(response){
+              console.log('image uploaded successfully');
+            }, function(response){
+              console.log('something went wrong when uploading');
+            });
+        }
+
+        function error(msg){
+            console.log(msg);
+        }
+
+        $scope.snap = function(){
+            var options = {
+              quality: 100,
+              destinationType: Camera.DestinationType.DATA_URL
+            };
+            navigator.camera.getPicture(success, error, options);
+        };
+
+        $scope.choose = function(){
+            var options = {
+              quality: 100,
+              sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+              destinationType: Camera.DestinationType.DATA_URL
+            };
+            navigator.camera.getPicture(success, error, options);
+        };
     })
 
     .controller('FriendsCtrl', function($scope, Note){
